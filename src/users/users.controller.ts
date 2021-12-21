33b73abf5d9d0e48e634b75d94358a54af1d5c12
chandanch <chandan.cec107@gmail.com>
2021-12-21 21:15:59 +0530

    create user dto and route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserDto } from './dtos/user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    @Post('/signup')
+    createUser(@Body() body: UserDto) {
+        return { ...body, status: 'USer Created' };
+    }
+}
