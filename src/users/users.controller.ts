bf4cfa8a474662dba54cf8672bf4fb525d86986b
chandanch <chandan.cec107@gmail.com>
2021-12-25 13:49:14 +0530

    get user by id with search by email and delete user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Query,
+} from '@nestjs/common';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +17,21 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: UserDto) {
         return this.usersService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findUsers(@Query('email') email: string, @Query('status') status: string) {
+        console.log(status);
+        return this.usersService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id));
     }
 }
