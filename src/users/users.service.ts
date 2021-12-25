51a1c359b873b9354d790d3591ff09c037a2493e
chandanch <chandan.cec107@gmail.com>
2021-12-25 11:09:23 +0530

    add update & remove method with use of partials :star:

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -18,4 +18,33 @@ export class UsersService {
         const status = await this.repo.save(user);
         return status;
     }
+
+    findOne(id: number) {
+        return this.repo.findOne(id);
+    }
+
+    find(email: string) {
+        return this.repo.find({ email });
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        // get the user entity first
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('User Not Found');
+        }
+        // copy all the properties and values from attrs using Object.Assign()
+        Object.assign(user, attrs);
+
+        return this.repo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('User Not Found');
+        }
+
+        return this.repo.remove(user);
+    }
 }
