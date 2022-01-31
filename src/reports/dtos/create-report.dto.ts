66ecbe1c30429a0dfcf1173605c832c134dac5d6
chandanch <chandan.cec107@gmail.com>
2022-01-31 09:46:59 +0530

    add report entity and dto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,15 @@
+export class CreateReportDTO {
+    make: string;
+
+    model: string;
+
+    year: number;
+
+    lat: number;
+
+    lng: number;
+
+    price: number;
+
+    mileage: number;
+}
