efc2e472690048144251d1bb4df50c850872cd77
chandanch <chandan.cec107@gmail.com>
2022-02-02 22:48:26 +0530

    transform/add new property into dto using transform decorator

--- src/reports/dtos/report.dto.ts
+++ src/reports/dtos/report.dto.ts
@@ -1,1 +1,28 @@
-export class ReportDTO {}
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDTO {
+    @Expose()
+    make: string;
+
+    @Expose()
+    model: string;
+
+    @Expose()
+    year: number;
+
+    @Expose()
+    lat: number;
+
+    @Expose()
+    lng: number;
+
+    @Expose()
+    price: number;
+
+    @Expose()
+    mileage: number;
+
+    @Expose()
+    @Transform(({ obj }) => obj.user.id)
+    userId: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/users.entity';
+import { Serializer } from 'src/interceptors/serializer.interceptor';
+import { ReportDTO } from './dtos/report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,6 +13,7 @@ export class ReportsController {
 
     @Post()
     @UseGuards(AuthGuard)
+    @Serializer(ReportDTO)
     addReport(@Body() body: CreateReportDTO, @CurrentUser() user: User) {
         return this.reportsService.create(body, user);
     }
